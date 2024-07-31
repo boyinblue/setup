3e06f381d2e9747156ccac868a037b04cb9371ee
parksejin <parksejin@rpi-home>
2024-08-01 02:26:24 +0900

    Update

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+#Network
+sudo apt-get install -y net-tools wget curl
+
+#Servers
+sudo apt-get install -y samba mailutils proftpd ssmtp xrdp
+
+#Editor
+sudo apt-get install -y vim gedit
+
+#Develop
+sudo apt-get install -y git hub gcc bmake python3 
+
+

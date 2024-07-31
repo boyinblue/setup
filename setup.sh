65186fa135737675a81a712cdcbbe180d8247bf2
boyinblue <boyinblue@naver.com>
2024-08-01 03:20:28 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+set -e
 
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get dist-upgrade
 
 #Network
 sudo apt-get install -y net-tools wget curl
 
 #Servers
-sudo apt-get install -y samba mailutils proftpd ssmtp xrdp ssh apache2 php mysql
+sudo apt-get install -y samba mailutils proftpd-core ssmtp xrdp ssh
+sudo apt-get install -y apache2 php mysql-server
 
 #Editor
 sudo apt-get install -y vim gedit
@@ -20,3 +23,5 @@ sudo apt-get install -y git hub
 sudo apt-get install -y gcc bmake
 sudo apt-get install -y python3 python3-pip python-is-python3
 
+sudo apt-get autoremove
+sudo apt-get autoclean
